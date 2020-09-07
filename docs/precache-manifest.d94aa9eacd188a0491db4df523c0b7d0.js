2a88be03f1544c2fe2d21683f3a16b3160fb9fa5
root <root@Frank-PC.localdomain>
2020-09-07 14:59:42 -0700

    docs

---
+++ docs/precache-manifest.d94aa9eacd188a0491db4df523c0b7d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b2f4ffb5bdc1238011b2a7732bb412",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "b82699817d6477b3d8a5",
+    "url": "/website/static/js/2.c30da478.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/website/static/js/2.c30da478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54fc6f82ba4338a0a40c",
+    "url": "/website/static/js/main.c3e7b5cf.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  }
+]);
